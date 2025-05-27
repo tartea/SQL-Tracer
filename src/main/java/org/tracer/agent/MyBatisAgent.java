e732163d2918edcf5c72e39c529adf1b75f853a9
tartea <1262466460@qq.com>
2025-05-27 13:29:40 +0800

    Update MyBatisAgent.java

--- src/main/java/org/tracer/agent/MyBatisAgent.java
+++ src/main/java/org/tracer/agent/MyBatisAgent.java
@@ -1,17 +1,19 @@
 package org.tracer.agent;
 
+import org.tracer.logger.LoggerUtil;
+
 import java.lang.instrument.Instrumentation;
+import java.util.HashMap;
+import java.util.Map;
 
 public class MyBatisAgent {
+    
+    
     public static void premain(String args, Instrumentation inst) {
         System.out.println("MyBatis SQL Capture Agent 已加载！");
 
-        if (args != null && !args.isEmpty()) {
-            System.out.println("Received agent args: " + args);
-            parseArgs(args);
-        } else {
-            System.out.println("No arguments provided.");
-        }
+       // 解析参数
+        parseArgs(args);
 
         boolean canRetransform = inst.isRetransformClassesSupported() && inst.isRedefineClassesSupported();
         if (canRetransform) {
@@ -28,14 +30,22 @@ public class MyBatisAgent {
      * 解析参数（格式：key1=value1,key2=value2）
      */
     private static void parseArgs(String agentArgs) {
-        String[] pairs = agentArgs.split(",");
-        for (String pair : pairs) {
-            String[] entry = pair.split("=", 2); // 最多分割成两部分
-            if (entry.length == 2) {
-                String key = entry[0].trim();
-                String value = entry[1].trim();
-                System.out.println("Key: " + key + ", Value: " + value);
+        Map<String, Object> params = new HashMap<>();
+        if (agentArgs != null && !agentArgs.isEmpty()) {
+            System.out.println("Received agent args: " + agentArgs);
+            String[] pairs = agentArgs.split(",");
+            for (String pair : pairs) {
+                String[] entry = pair.split("=", 2); // 最多分割成两部分
+                if (entry.length == 2) {
+                    String key = entry[0].trim();
+                    String value = entry[1].trim();
+                    params.put(key, value);
+                }
             }
+        } else {
+            System.out.println("No arguments provided.");
         }
+        LoggerUtil.builder(params);
     }
+    
 }
